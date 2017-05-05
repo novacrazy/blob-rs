5ea9175fe7790b2b160cda2b056141e8649dadcd
Aaron Trent <novacrazy@gmail.com>
2017-05-05 14:09:14 -0500

    Update example with arbitrary write

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,13 +1,19 @@
 extern crate blob;
 
+use std::io::Write;
+
 use blob::Blob;
 
 const DATA: [u8; 5] = [0x1, 0x2, 0x3, 0x4, 0x5];
 
 fn main() {
-    let my_blob = Blob::from(&DATA[..]);
+    let mut my_blob = Blob::from(&DATA[..]);
 
     println!("{}", my_blob);
 
     assert_eq!(my_blob, DATA);
+
+    write!(my_blob, "Testing").unwrap();
+
+    println!("{}", my_blob);
 }
